5d75623f4d9f1da2c2e10aaf4f12565c5efbabd6
Paweennawat Sukruam <Paweennawat.s@kkumail.com>
2024-09-12 12:21:34 +0000

    update ProductForm

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -12,55 +12,67 @@ function ProductForm() {
                             {/* <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a> */}
-                            <form class="row g-3 needs-validation" novalidate>
-                                <div class="col-md-4 position-relative">
-                                    <label for="validationTooltip01" class="form-label">First name</label>
-                                    <input type="text" class="form-control" id="validationTooltip01" value="Mark" required />
+                            <form class="row g-2 needs-validation" novalidate>
+                                <div class="col-md-3 position-relative">
+                                    <label for="validationTooltip01" class="form-label">Name</label>
+                                    <input type="text" class="form-control" id="validationTooltip01" value="Pepsi" required />
                                     <div class="valid-tooltip">
                                         Looks good!
                                     </div>
                                 </div>
-                                <div class="col-md-4 position-relative">
-                                    <label for="validationTooltip02" class="form-label">Last name</label>
-                                    <input type="text" class="form-control" id="validationTooltip02" value="Otto" required />
-                                    <div class="valid-tooltip">
-                                        Looks good!
-                                    </div>
-                                </div>
-                                <div class="col-md-4 position-relative">
-                                    <label for="validationTooltipUsername" class="form-label">Username</label>
-                                    <div class="input-group has-validation">
-                                        <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
-                                        <input type="text" class="form-control" id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend" required />
-                                        <div class="invalid-tooltip">
-                                            Please choose a unique and valid username.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 position-relative">
-                                    <label for="validationTooltip03" class="form-label">City</label>
+                                <div class="col-md-2 position-relative">
+                                    <label for="validationTooltip03" class="form-label">Price</label>
                                     <input type="text" class="form-control" id="validationTooltip03" required />
                                     <div class="invalid-tooltip">
-                                        Please provide a valid city.
+                                        Please provide a valid Price.
                                     </div>
                                 </div>
-                                <div class="col-md-3 position-relative">
-                                    <label for="validationTooltip04" class="form-label">State</label>
+                                <div class="col-md-2 position-relative">
+                                    <label for="validationTooltip02" class="form-label">Quantity</label>
+                                    <input type="text" class="form-control" id="validationTooltip02" required />
+                                    <div class="valid-tooltip">
+                                        Please provide a valid Quantity.
+                                    </div>
+                                </div>
+                                <div class="col-md-2 position-relative">
+                                    <label for="validationTooltip04" class="form-label">Category</label>
                                     <select class="form-select" id="validationTooltip04" required>
                                         <option selected disabled value="">Choose...</option>
-                                        <option>...</option>
+                                        <option>Food</option>
+                                        <option>Drink</option>
+                                        <option>Electronic</option>
                                     </select>
                                     <div class="invalid-tooltip">
-                                        Please select a valid state.
+                                        Please select a Category.
                                     </div>
                                 </div>
                                 <div class="col-md-3 position-relative">
-                                    <label for="validationTooltip05" class="form-label">Zip</label>
-                                    <input type="text" class="form-control" id="validationTooltip05" required />
-                                    <div class="invalid-tooltip">
-                                        Please provide a valid zip.
-                                    </div>
+                                    <label for="validationTooltip02" class="form-label">Description</label>
+                                    <textarea type="text" class="form-control" id="validationTooltip02" style={{ height: '150px' }} rows="4" required />
+                                </div>      
+                                <div class="col-md-3 position-relative">
+                                    <label for="validationTooltip05" class="form-label">Manufacturer</label>
+                                    <input type="text" class="form-control" id="validationTooltip05"/>
                                 </div>
+                                <div class="col-md-2 position-relative">
+                                    <label for="validationTooltip05" class="form-label">Warranty Period</label>
+                                    <input type="text" class="form-control" id="validationTooltip05"/>
+                                </div>
+                                <div class="col-md-5 position-relative">
+                                    <label for="validationTooltip02" class="form-label">Image Url</label>
+                                    <textarea type="text" class="form-control" id="validationTooltip02" style={{ height: '125px' }} rows="4"/>
+                                </div> 
+                                {/* <div class="col-md-2 position-relative">
+                                    <label for="validationTooltip04" class="form-label">Rating</label>
+                                    <select class="form-select" id="validationTooltip04" required>
+                                        <option selected disabled value="">Rate...</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </div> */}
                                 <div class="col-12">
                                     <button class="btn btn-primary" type="submit">Submit form</button>
                                 </div>
